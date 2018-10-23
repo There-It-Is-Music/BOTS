44a2e67aa04fd6b7f1ebf226ad2fc619cb396767
There-It-Is-Music <42722182+There-It-Is-Music@users.noreply.github.com>
2018-10-23 02:02:32 -0400

    Update SerenityBar.py

--- SerenityBar.py
+++ SerenityBar.py
@@ -116,8 +116,8 @@ def the_bot(id, passw):
             print("Wrong mode!")
 
 p = Pool()
-p.starmap(the_bot, [("KavaKaze", "@ndr3w0414Mln")])
-#p = Process(target=the_bot, args=["KavaKaze", "@ndr3w0414Mln"])
+p.starmap(the_bot, [("serenitycoffeekava ", "@ndr3w0414Mln")])
+#p = Process(target=the_bot, args=["serenitycoffeekava ", "@ndr3w0414Mln"])
 #p.start()
 #p.join()
 #pool.apply_async(the_bot)
